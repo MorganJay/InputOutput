854ec64c4ccf242b3522124f1855386d6801eb0e
James Morgan O <MorganJay@users.noreply.github.com>
2020-12-15 23:46:36 +0100

    Haha

--- InputOutput/Program.cs
+++ InputOutput/Program.cs
@@ -1,37 +1,82 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Threading;
 
 // Use this blog website generator to create an html file that asks for a user name, file name, blog content and date published.
+//collect name of file
+//create file
+//add a heading
+// enter the body
+// enter my footer
 // Write a method that appends to the top lines of a file, to write time logs into a file. it logs every 5seconds....no hoodlum in site.
 namespace InputOutput
 {
     internal class Program
     {
+        private static DateTime _today = DateTime.Now;
+
         private static void Main()
         {
-            const string filePath = "C:\\New folder\\daniel.html";
-            LowLevelWrite(filePath);
-            var innerText = File.ReadAllText(filePath);
+            GreetUser();
+            Console.WriteLine("\nSo, what's your name? This will serve as your username");
+            var userName = Console.ReadLine();
+            Console.WriteLine("What would you like the file name to be?");
+            var blogName = Console.ReadLine();
+            var filePath = $"C:\\Users\\JamesMorgan\\Desktop\\SystemIO\\{blogName}.html";
+            File.AppendAllText(filePath, $"<h1>Hi my name is {userName} Welcome to my blog</h1>");
 
-            //Console.WriteLine(innerText);
-            Console.ReadLine();
+            Console.WriteLine("\nEnter the headline for your blog post");
+            var heading = Console.ReadLine();
+            File.AppendAllText(filePath, $"<h2>{heading}</h2>");
+
+            var allContent = new List<string>();
+            Console.WriteLine("\nNow type the content of the post");
+            var typing = true;
+            do
+            {
+                var body = Console.ReadLine();
+                allContent.Add($"<p>{body}</p>");
+                Console.WriteLine("Do you wish to add more content?");
+                var typingChoice = Console.ReadLine().ToLower();
+                if (typingChoice == "no")
+                {
+                    typing = false;
+                }
+            } while (typing);
+
+            File.AppendAllLines(filePath, allContent);
+            File.AppendAllText(filePath, $"<p>Date Published: {_today:f}</p>");
+            Console.WriteLine("Nicely done. Now let's see your new website! You can share with your friends");
+            LaunchFile(filePath);
         }
 
-        private static void LowLevelWrite(string path)
+        private static void LaunchFile(string filePath) // to open the file after the user has completed the blog
         {
-            //var str = new StreamWriter(path);
-            //str.WriteLine("This is from me");
-            //str.WriteLine("This is amazing");
-            using (var str = new StreamWriter(path))
+            Console.WriteLine("\nAny minute now...");
+            Thread.Sleep(2000);
+            Process.Start(filePath);
+        }
+
+        private static void GreetUser()
+        {
+            Console.WriteLine($"\t\t\t\t\t\t\t\t\t\t\t        {_today:f}");
+            if (_today.Hour < 12)
             {
-                //str.WriteLine("This is from me");
-                //str.WriteLine("This is amazing");
-                str.WriteLine("<h1>Hello Daniel!</h1>");
-                str.Close();
+                Console.WriteLine("Good morning this is a blog generator program for that topic you've always wanted to post");
+                Console.WriteLine("I hope you enjoy using it as much as I enjoyed building it for you");
             }
-            //ile.AppendAllText(path, "This is an appended line");
-            //File.WriteAllLines(path, "");
-            File.ReadAllText(path);
+            else if (_today.Hour >= 12 && _today.Hour <= 18)
+            {
+                Console.WriteLine("Good afternoon this is a blog generator program for that topic you've always wanted to post");
+                Console.WriteLine("I hope you enjoy using it as much as I enjoyed building it for you");
+            }
+            else
+            {
+                Console.WriteLine("Good evening this is a blog generator program for that topic you've always wanted to post");
+                Console.WriteLine("I hope you enjoy using it as much as I enjoyed building it for you");
+            }
         }
     }
 }
