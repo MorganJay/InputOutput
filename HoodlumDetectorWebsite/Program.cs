1d790039e1dc4d8e60fe9bc030715fd99bd8e23e
James Morgan O <MorganJay@users.noreply.github.com>
2020-12-16 12:54:24 +0100

    Almost there

---
+++ HoodlumDetectorWebsite/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+// Write a method that appends to the top lines of a file, to write time logs into a file. it logs every 5seconds....no hoodlum in sight.
+namespace HoodlumDetectorWebsite
+{
+    internal class Program
+    {
+        private static void Main() // not complete
+        {
+            const string message = "<h1>No hoodlums in sight</h1>";
+            const string filePath = "C:\\Users\\JamesMorgan\\Desktop\\hoodlums.html";
+            var dateTime = DateTime.Now;
+            File.AppendAllText(filePath, $"{message}<p>{dateTime:T}</p>");
+            LaunchFile(filePath);
+
+            while (true)
+            {
+                Thread.Sleep(5000);
+                var currentTime = DateTime.Now;
+                var newMessage = $"{message}<p>{currentTime:T}</p>";
+                File.AppendAllText(filePath, newMessage);
+            }
+        }
+
+        private static void LaunchFile(string filePath) // to open the file after the user has completed the blog
+        {
+            Console.WriteLine("\nAny minute now...");
+            Thread.Sleep(1000);
+            Process.Start(filePath);
+        }
+
+        private static Stack<string> PrependMessages(List<string> messages)
+        {
+            var messageStack = new Stack<string>();
+            foreach (var message in messages)
+            {
+                var today = DateTime.Now;
+                var newMessage = $"<h1>{message}</h1><p>{today:T}</p>";
+                Console.WriteLine(newMessage);
+                //messages.Add(newMessage);
+            }
+            return messageStack;
+        }
+
+        private static string PrepareMessages(string rawMessage)
+        {
+            var newMessage = rawMessage;
+            return newMessage;
+        }
+
+        //private static IEnumerable<string> PrependMessage(IEnumerable<string> messages)
+        ////{
+        //    return messagesStack;
+        //}
+    }
+}
--- InputOutput/Program.cs
+++ InputOutput/Program.cs
@@ -10,12 +10,12 @@ using System.Threading;
 //add a heading
 // enter the body
 // enter my footer
-// Write a method that appends to the top lines of a file, to write time logs into a file. it logs every 5seconds....no hoodlum in site.
+
 namespace InputOutput
 {
     internal class Program
     {
-        private static DateTime _today = DateTime.Now;
+        private static readonly List<string> AllContent = new List<string>();
 
         private static void Main()
         {
@@ -25,58 +25,221 @@ namespace InputOutput
             Console.WriteLine("What would you like the file name to be?");
             var blogName = Console.ReadLine();
             var filePath = $"C:\\Users\\JamesMorgan\\Desktop\\SystemIO\\{blogName}.html";
-            File.AppendAllText(filePath, $"<h1>Hi my name is {userName} Welcome to my blog</h1>");
+            File.AppendAllText(filePath, $"<h2>Hi my name is {userName} Welcome to my blog</h2>");
 
-            Console.WriteLine("\nEnter the headline for your blog post");
+            Console.WriteLine("\nWhat's the topic for your blog post");
             var heading = Console.ReadLine();
-            File.AppendAllText(filePath, $"<h2>{heading}</h2>");
+            File.AppendAllText(filePath, $"<h1>{heading}</h1>");
 
-            var allContent = new List<string>();
-            Console.WriteLine("\nNow type the content of the post");
-            var typing = true;
-            do
-            {
-                var body = Console.ReadLine();
-                allContent.Add($"<p>{body}</p>");
-                Console.WriteLine("Do you wish to add more content?");
-                var typingChoice = Console.ReadLine().ToLower();
-                if (typingChoice == "no")
-                {
-                    typing = false;
-                }
-            } while (typing);
-
-            File.AppendAllLines(filePath, allContent);
-            File.AppendAllText(filePath, $"<p>Date Published: {_today:f}</p>");
+            Console.WriteLine("\nNow start typing the content of your post");
+            CollectContent();
+            File.AppendAllLines(filePath, AllContent);
+            DatePublished(filePath);
             Console.WriteLine("Nicely done. Now let's see your new website! You can share with your friends");
             LaunchFile(filePath);
         }
 
-        private static void LaunchFile(string filePath) // to open the file after the user has completed the blog
+        private static void BoilerplateTop(string filepath)
         {
-            Console.WriteLine("\nAny minute now...");
-            Thread.Sleep(2000);
-            Process.Start(filePath);
+            var top = @" <!DOCTYPE html>
+            <html lang = ""en"">
+            <head>
+            <link rel = ""shortcut icon"" href = ""./img/c-sharp-c-logo-02F17714BA-seeklogo.com.png"" type = ""image/x-icon"" >
+
+                 < title > My Blog </ title >
+
+                    < !--Bootstrap core CSS -->
+
+                    < link href = ""vendor / bootstrap / css / bootstrap.min.css"" rel = ""stylesheet"" >
+
+                             < !--Custom fonts for this template-- >
+                             < link href = ""vendor/fontawesome-free/css/all.min.css"" rel = ""stylesheet"" type = ""text/css"" >
+                             < link href = 'https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel = 'stylesheet' type = ""text/css"">
+                             < link href = 'https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+                               rel = 'stylesheet' type = 'text/css'>
+
+                             < !--Custom styles for this template-- >
+                             < link href = ""css/clean-blog.min.css"" rel = ""stylesheet"">
+                           </ head >
+                           < body >
+
+                             < !--Page Header-- >
+
+                             < header class="masthead" style="background - image: url('/img/net-core-background-services_870_220.jpg')">
+
+                          < div class="overlay"></div>
+
+                        <div class="container">
+
+                          <div class="row">
+
+                            <div class="col-lg-8 col-md-10 mx-auto">
+
+                              <div class="site-heading">
+
+                                <h1>Clean Blog</h1>
+
+                                <span class="subheading">private A Blog private Theme by Start Bootstrap</span>
+
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </header>
+
+                      <!-- Main Content -->
+
+                      <div class="container">
+
+                        <div class="row">
+
+                          <div class="col-lg-8 col-md-10 mx-auto">
+
+                            <div class="post-preview">
+
+                              <private a href = "post.html" >
+
+                                < h2 class="post-title">
+
+                                  private Man must explore, and this is exploration at its greatest
+                                </h2>
+
+                                <h3 class="post-subtitle">
+
+                                  private Problems look private mighty small from 150 private miles up
+                                </h3>
+                              </a>
+
+                              <p class="post-meta">private Posted by
+
+                                <a href= "#" > Start Bootstrap</a>
+                                on September 24, 2019
+                              </p>
+                            </div>
+
+                            <!-- Footer -->
+                            <footer>
+
+                              <div class="container">
+
+                                <div class="row">
+
+                                  <div class="col-lg-8 col-md-10 mx-auto">
+
+                                    <ul class="list-inline text-center">
+
+                                      <li class="list-inline-item">
+
+                                        <private a href = "#" >
+
+                                          < span class="fa-stack fa-lg">
+
+                                            <i class="fas fa-circle fa-stack-2x"></i>
+
+                                            <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+
+                                          </span>
+                                        </a>
+                                      </li>
+
+                                      <li class="list-inline-item">
+
+                                        <private a href = "#" >
+
+                                          < span class="fa-stack fa-lg">
+
+                                            <i class="fas fa-circle fa-stack-2x"></i>
+
+                                            <i class="fab fa-github fa-stack-1x fa-inverse"></i>
+
+                                          </span>
+                                        </a>
+                                      </li>
+                                    </ul>
+
+                                    <p class="copyright text-muted">Copyright &copy; private Your Website 2020</p>
+
+                                  </div>
+                                </div>
+                              </div>
+                            </footer>
+
+                            <!-- private Bootstrap core JavaScript -->
+
+                            <private script src = "vendor/jquery/jquery.min.js" ></ script >
+                            < script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+                            <!-- private Custom scripts for this template -->
+
+                            <private script src = "js/clean-blog.min.js" ></ script >
+
+                    </ body >
+
+                    </ html > ";
         }
 
-        private static void GreetUser()
+    private static void CollectContent()
+    {
+        var body = Console.ReadLine();
+        AllContent.Add($"<p>{body}</p>");
+
+        var typing = true;
+        do
         {
-            Console.WriteLine($"\t\t\t\t\t\t\t\t\t\t\t        {_today:f}");
-            if (_today.Hour < 12)
+            Console.Write("Do you wish to add more content? ");
+            var typingChoice = Console.ReadLine().ToLower();
+            switch (typingChoice)
             {
-                Console.WriteLine("Good morning this is a blog generator program for that topic you've always wanted to post");
-                Console.WriteLine("I hope you enjoy using it as much as I enjoyed building it for you");
+                case "no":
+                    typing = false;
+                    break;
+
+                case "yes":
+                    Console.WriteLine("=> ");
+                    body = Console.ReadLine();
+                    AllContent.Add($"<p>{body}</p>");
+                    break;
+
+                default:
+                    Console.WriteLine("You entered a wrong input which terminates the program.\n");
+                    typing = false;
+                    break;
             }
-            else if (_today.Hour >= 12 && _today.Hour <= 18)
-            {
-                Console.WriteLine("Good afternoon this is a blog generator program for that topic you've always wanted to post");
-                Console.WriteLine("I hope you enjoy using it as much as I enjoyed building it for you");
-            }
-            else
-            {
-                Console.WriteLine("Good evening this is a blog generator program for that topic you've always wanted to post");
-                Console.WriteLine("I hope you enjoy using it as much as I enjoyed building it for you");
-            }
+        } while (typing);
+    }
+
+    private static void LaunchFile(string filePath) // to open the file after the user has completed the blog
+    {
+        Console.WriteLine("\nAny minute now...");
+        Thread.Sleep(2000);
+        Process.Start(filePath);
+    }
+
+    private static void GreetUser()
+    {
+        var today = DateTime.Now;
+        Console.WriteLine($"\t\t\t\t\t\t\t\t\t\t\t        {today:D}");
+        if (today.Hour < 12)
+        {
+            Console.WriteLine("Good morning this is a blog generator program for that topic you've always wanted to post");
+            Console.WriteLine("I hope you enjoy using it as much as I enjoyed building it for you");
+        }
+        else if (today.Hour >= 12 && today.Hour <= 18)
+        {
+            Console.WriteLine("Good afternoon this is a blog generator program for that topic you've always wanted to post");
+            Console.WriteLine("I hope you enjoy using it as much as I enjoyed building it for you");
+        }
+        else
+        {
+            Console.WriteLine("Good evening this is a blog generator program for that topic you've always wanted to post");
+            Console.WriteLine("I hope you enjoy using this as much as I enjoyed building it for you");
         }
     }
+
+    private static void DatePublished(string filepath)
+    {
+        var now = DateTime.Now;
+        File.AppendAllText(filepath, $"<p>Published on: {now:f}</p>");
+    }
+}
 }
